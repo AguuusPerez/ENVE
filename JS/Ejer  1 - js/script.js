c2ade2db57067e5b00ac685b5811f72f5fac0576
Agustina Perez <agustinandrea.p@gmail.com>
2022-01-07 21:55:55 -0300

    new commit

---
+++ JS/Ejer  1 - js/script.js
@@ -0,0 +1,41 @@
+  /*function num(){
+
+let numeros = [];
+
+while (numeros != 'q'){
+numeros = prompt("Ingrese un numero: \nPara salir ingrese 'q' ");
+}        
+ alert('Saliste del programa');     
+
+numeros = prompt("Ingrese un numero: \nPara salir ingrese 'q' ");
+alert('El numero ingresado multiplicado por dos es :'  + (numeros * 2));    
+}
+        
+num();*/
+        
+function num(){
+let numeros = [];
+    
+do {
+info = prompt("Ingrese un numero: \nPara salir ingrese 'q' ")
+numeros.push(info);    
+} while (info != 'q')
+alert('Saliste del programa');
+}
+    
+num();
+    
+// multip * 2 los num ingresados    
+/* function multip(n){
+    
+ alert('El numero ingresado multiplicado por dos es :' + (info * 2));
+ }
+            
+multip(numeros);
+
+
+// numeros primos    
+function numPrimos(n){
+if
+} */
+    
